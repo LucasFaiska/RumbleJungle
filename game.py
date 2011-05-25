35d7e0517b759415357bc25999c1a67f628a6adb
Rodolfo Stangherlin <rodolfo.3@gmail.com>
2011-05-25 19:35:25 -0300

    Adição de testes e implementação de jogo básico para iniciar implementação de servidor em sockets.

--- game.py
+++ game.py
@@ -10,6 +10,9 @@ class Piece:
 	def can_catch(self, piece):
 		return True
 
+class InvalidAction(Exception):
+	pass
+
 from copy import copy
 class BasicGame:
 	'''
@@ -34,6 +37,7 @@ class BasicGame:
 		self._pieces = {}
 		self._board = copy(self.INITIAL_BOARD)
 		self._players = [hostPlayer,]
+		self._turn = 0
 
 	def join(self, player):
 		# TODO check max game players
@@ -47,8 +51,34 @@ class BasicGame:
 		self._pieces[ (0,0) ] = Piece(player0)
 		self._pieces[ (2,2) ] = Piece(player1)
 
+	def playerTurn(self):
+		playerNo = self._turn % len(self._players)
+		return self._players[playerNo]
+
+	def _checkTurn(self, piece):
+		if piece._player != self.playerTurn():
+			raise InvalidAction()
+
+	def _checkMove(self, initial, final):
+		if abs(initial[0]-final[0] + initial[1]-final[1]) > 1:
+			raise InvalidAction()
+
 	def move(self, initial, final):
 		initial_piece = self._pieces.get(initial)
 		final_piece = self._pieces.get(final)
-		# FIXME
-		
+
+		if initial_piece is None: raise InvalidAction()
+		self._checkTurn(initial_piece)
+		self._checkMove(initial, final)
+
+		self._pieces.pop(initial) #remove
+		if final_piece: self._pieces.pop(final) #remove, if exists
+		self._pieces[final] = initial_piece
+		self._turn += 1
+		return True
+
+	def win(self):
+		player = set([piece._player for piece in self._pieces.values()])
+		if len(player) == 1: return player.pop()
+		return None
+
--- tests/test_game.py
+++ tests/test_game.py
@@ -1,6 +1,6 @@
 
 import unittest
-from game import BasicGame, Player
+from game import BasicGame, Player, InvalidAction
 
 class TestPlayer(unittest.TestCase):
 	'''
@@ -49,6 +49,66 @@ class TestBasicGame(unittest.TestCase):
 		Pieces on the board
 		'''
 		game = self.game
-		self.assertTrue(game._pieces[(0,0)]._player == self.player0)
-		self.assertTrue(game._pieces[(2,2)]._player == self.player1)
+		self.assertEqual(game._pieces[(0,0)]._player, self.player0)
+		self.assertEqual(game._pieces[(2,2)]._player, self.player1)
+
+	def test_turn(self):
+		'''
+		After moving, it changes the player turn (not when runs 
+		"playerTurn()")
+		'''
+		game = self.game
+		self.assertEqual(game.playerTurn(), self.player0)
+		self.assertEqual(game.playerTurn(), self.player0)
+		game.move((0,0), (0,1))
+		self.assertEqual(game.playerTurn(), self.player1)
+		game.move((2,2), (2,1))
+		self.assertEqual(game.playerTurn(), self.player0)
+
+	def test_move(self):
+		'''
+		Move pieces on the board
+		'''
+		game = self.game
+		self.assertTrue(game.move(initial=(0,0), final=(0,1)))
+		self.assertEqual(game._pieces[(0,1)]._player, self.player0)
+
+	def test_move_without_piece(self):
+		'''
+		Raises exception when no piece in initial place
+		'''
+		game = self.game
+		self.assertRaises(InvalidAction, game.move, (1,1), (2,1))
+
+	def test_move_wrong_player(self):
+		'''
+		Raises exception when wrong player try a movement
+		(2,2) is a player1 piece, but player0 starts
+		'''
+		game = self.game
+		self.assertRaises(InvalidAction, game.move, (2,2), (2,1))
+
+	def test_move_wrong_destination(self):
+		'''
+		Game not allow go 2 squares long
+		'''
+		game = self.game
+		self.assertRaises(InvalidAction, game.move, (0,0), (0,2))
+		self.assertRaises(InvalidAction, game.move, (0,0), (2,0))
+		self.assertRaises(InvalidAction, game.move, (0,0), (2,2))
+
+	def test_win(self):
+		'''
+		Test when some player win the game
+		'''
+		game = self.game
+		self.assertEqual(game.win(), None)
+		game.move( (0,0), (0,1) )
+		self.assertEqual(game.win(), None)
+		game.move( (2,2), (2,1) )
+		self.assertEqual(game.win(), None)
+		game.move( (0,1), (1,1) )
+		self.assertEqual(game.win(), None)
+		game.move( (2,1), (1,1) ) # eat and win!
+		self.assertEqual(game.win(), self.player1)
 
